45855122ff9d497f8a226e1631ed676f3b43111a
Edmar Nunes <edmar.nunes@kepha.com.br>
2025-12-10 16:58:34 -0300

    chore(reports): configurar Allure com limpeza automatica e abertura do relatorio

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,21 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://restful-booker.herokuapp.com",
     watchForFileChanges: false,
     defaultCommandTimeout: 15000,
+    supportFile: "cypress/support/e2e.js",
+    
     reporter: "junit",
     reporterOptions: {
-      mochaFile: "reports/results-[hash].xml",
+      mochaFile: "reports/results.xml",
       toConsole: true
     },
+
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       return config;
     }
   }
